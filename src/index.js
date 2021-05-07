726bbf6991824f013245ea8f16ce89e3c26456db
Valerian Rykov <Valerra2@gmail.com>
2021-05-07 17:08:56 +0300

    test

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import './styles.css';
+import './js/test.js';
+//import print from './js/test';
+
+// console.log(print);
+
+// print('Not Hi');
+
+//import { text, printText } from './js/test';
+//console.log(text);
+//printText('J - S');
+
+import * as object from './js/test';
+
+console.log(object.text);
+
+object.printText('JeyEs');
